e215aac0d4c4587027a76923e5e614e9ee8d4ef5
ArifinPangestu <arifingokil2005@gmail.com>
2025-05-31 22:04:06 +0700

    uas-selesai

--- lib/data_saya.dart
+++ lib/data_saya.dart
@@ -1,4 +1,4 @@
 class DataSaya {
-   static String nama = "Kudut";
-    static String gambar = "assets/mahasiswa/kudut.png";
+  static String nama = "Arifin-Pangestu";
+  static String gambar = "assets/arifin.jpg";
 }
